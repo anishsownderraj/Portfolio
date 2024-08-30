e7dfbf8677d54857147a1c2d21cc870c098b9314
anishsownderraj <toanish21@gmail.com>
2024-08-30 15:44:14 -0700

    Add main jsx file

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+// dependencies
+import ReactDOM from 'react-dom/client';
+// components
+import { App } from './App';
+// styling
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
